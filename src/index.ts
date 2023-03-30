58279d3841bec63340e42762caf447381d68ffe8
Rafael Santos <rafaelnsantos1@gmail.com>
2023-03-30 02:22:11 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -12,22 +12,11 @@ const clientId = process.env.DISCORD_CLIENT_ID as string;
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-(async () => {
-  const rest = new REST({ version: "10" }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
-  try {
-    console.log(
-      `Started refreshing ${comandos.length} application (/) commands.`
-    );
-    // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationCommands(clientId), {
-      body: comandos,
-    });
-  } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
-  }
-})();
+rest.put(Routes.applicationCommands(clientId), {
+  body: comandos,
+}).then(() => console.log(`Refreshed ${comandos.length} application (/) commands.`))
 
 handleEvents(client);
 
